ff7fb4fa832dc944eb2ef9e88ba68d8fd3195f93
Nonetss <amorenolopezbarajas@gmail.com>
2025-02-07 12:08:04 +0100

    Introducimos el nuevo endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.api.v1.get_newsletter import router as correos_router
+from app.api.v1.summarize import router as summarize_router
 from app.core.database import Base, engine
 
 app = FastAPI()
@@ -11,6 +12,7 @@ Base.metadata.create_all(bind=engine)
 
 # Registrar los endpoints de correos
 app.include_router(correos_router, prefix="/api/v1")
+app.include_router(summarize_router, prefix="/api/v1", tags=["Summarize"])
 
 
 @app.get("/")

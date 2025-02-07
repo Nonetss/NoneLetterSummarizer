0c439e092a6c0dda5ae391918c010425d7c30cf0
Nonetss <amorenolopezbarajas@gmail.com>
2025-02-07 10:49:03 +0100

    Quitamos codigo innecesario

--- app/core/sesion.py
+++ app/core/sesion.py
@@ -1,25 +1,10 @@
-import email
 import imaplib
 import logging
-import os
-from email.header import decode_header
-from typing import Optional
 
-from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException
+from app.core.config import IMAP_PORT, IMAP_SERVER, PASSWORD, USERNAME
 
-load_dotenv()
-
-# Configura el nivel de logs para ver detalles en caso de errores
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-# Configuración de Proton Bridge (IMAP)
-IMAP_SERVER = os.getenv("IMAP_SERVER")
-IMAP_PORT = os.getenv("IMAP_PORT")
-USERNAME = os.getenv("USERNAME")
-PASSWORD = os.getenv("PASSWORD")
-FOLDER = os.getenv("FOLDER")
 
 
 # Iniciamos sesión en el correo
